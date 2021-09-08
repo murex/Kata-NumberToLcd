3f2676945120b8a8ebf57a26634649f56d06302d
Damien Menanteau <damien.menanteau@gmail.com>
2021-09-08 14:26:33 +0200

    Add "using namespace" to simplify function calls

--- cpp/test/LcdTest.cpp
+++ cpp/test/LcdTest.cpp
@@ -3,11 +3,13 @@
 
 #include <gtest/gtest.h>
 
+using namespace com::murex::kata::lcd;
+
 TEST(Lcd, DISABLED_acceptance_test)
 {
 	const auto expected =
 		"    _  _     _  _     _  _ \n"
 		"  | _|| |  | _|| |  | _|| |\n"
 		"  ||_ |_|  ||_ |_|  ||_ |_|\n";
-	EXPECT_EQ(expected, com::murex::kata::lcd::lcdConvert(120120120));
+	EXPECT_EQ(expected, lcdConvert(120120120));
 }
